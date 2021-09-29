8bf1d92b5c4a0d6f5690590ec833f4b03d21bcdb
Wasswa Richard <wasswa@awamo.com>
2021-09-29 09:28:45 +0300

    proxy requests

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,12 @@ module.exports = function(app) {
     app.use(
         createProxyMiddleware("/pay", {
             target: "https://topup.vercel.app",
-            secure: false,
             changeOrigin: true
         })
     );
     app.use(
         createProxyMiddleware("/widget", {
             target: "https://topup.vercel.app",
-            secure: false,
             changeOrigin: true
         })
     );
